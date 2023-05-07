fe1f531e9722298566583c82c07234c25f2a2337
KeyNGAdnil <571047757@qq.com>
2023-05-07 23:16:26 +0800

    init

---
+++ client/src/extension.ts
@@ -0,0 +1,77 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext, languages, CompletionItem, CompletionItemKind } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// 注册代码提示提供程序
+	// const completionProvider = languages.registerCompletionItemProvider('javascript', {
+	// 	provideCompletionItems() {
+	// 		// 创建自定义代码补全项
+	// 		const myCompletionItem = new CompletionItem('my');
+	// 		myCompletionItem.kind = CompletionItemKind.Keyword;
+
+	// 		// 返回代码补全项列表
+	// 		return [myCompletionItem];
+	// 	},
+	// });
+
+	// context.subscriptions.push(completionProvider);
+	// 服务端配置信息
+	// 对于 Node 形式的插件，只需要定义入口文件即可，vscode 会帮我们管理好进程的状态
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// 定义插件在什么时候生效
+		documentSelector: [{ scheme: 'file', language: 'javascript' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
---
+++ server/src/server.ts
@@ -0,0 +1,115 @@
+import {
+  createConnection,
+  TextDocuments,
+  ProposedFeatures,
+  InitializeParams,
+  CompletionItem,
+  CompletionItemKind,
+  TextDocumentPositionParams,
+  CompletionParams,
+  TextDocumentSyncKind,
+  InsertTextFormat,
+  Position,
+} from 'vscode-languageserver/node';
+
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+// 创建连接
+const connection = createConnection(ProposedFeatures.all);
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+
+// 监听文档变化
+documents.listen(connection);
+
+connection.onInitialize((params: InitializeParams) => {
+  return {
+    capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
+      completionProvider: {
+        resolveProvider: true,
+        triggerCharacters: ['.', 'm'],
+      },
+    },
+  };
+});
+
+// 提示项
+const completionItems: CompletionItem[] = [
+  {
+    label: 'tb',
+    kind: CompletionItemKind.Method,
+    detail: 'My custom method',
+    insertText: 'tb',
+  },
+  {
+    label: 'myMethod',
+    kind: CompletionItemKind.Method,
+    detail: 'My custom method',
+    insertText: 'myMethod()',
+  },
+  {
+    label: 'myProperty',
+    kind: CompletionItemKind.Method,
+    detail: 'My custom property',
+    insertText: 'myConfigData()',
+  },
+];
+const myCompletionItems: CompletionItem[] = [
+  {
+    label: 'my',
+    kind: CompletionItemKind.Method,
+    detail: 'My custom method',
+    insertText: 'my',
+  },
+];
+
+
+// 提示功能
+connection.onCompletion(
+  (params: CompletionParams): CompletionItem[] => {
+    const document = documents.get(params.textDocument.uri);
+    const position = params.position;
+
+    if (document) {
+      // const text = document.getText();
+
+      // 在此处根据具体逻辑判断是否触发代码补全
+      // 获取光标前的文本
+      const lineText = document.getText({
+        start: { line: position.line, character: 0 },
+        end: position,
+      });
+      if (lineText.endsWith('m')) {
+        console.log('触发了m');
+        console.log('myCompletionItems', myCompletionItems);
+        return myCompletionItems;
+      }
+      // 检查光标前的文本是否以 "my." 结尾
+      if (lineText.endsWith('my.')) {
+        console.log('触发了my');
+        // console.log('completionItems', completionItems);
+        return completionItems;
+      }
+      if (lineText.endsWith('tb.')) {
+        console.log('触发了tb');
+        return completionItems;
+      }
+      return []
+    }
+
+    return [];
+  }
+);
+// 设置选择时 补齐代码文档信息
+connection.onCompletionResolve((item) => {
+  if (item.label === 'myProperty') {
+    // 在这里根据需要设置补全项的详细信息
+    item.documentation = 'Documentation for my';
+    item.detail = 'Custom Completion - my';
+  }
+
+  return item;
+});
+// 启动 LSP 服务
+connection.listen();
